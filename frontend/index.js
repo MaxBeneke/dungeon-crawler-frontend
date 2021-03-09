3ac83362bea0187c2ec592779aac7d585cdcb538
Max Beneke <max.beneke@gmail.com>
2021-03-09 14:33:36 -0500

    Item pickups and battle logic started

--- frontend/index.js
+++ frontend/index.js
@@ -5,9 +5,12 @@ const battleContainer = document.querySelector('div#battle')
 const characterContainer = document.querySelector('div#character-container')
 const character = document.querySelector('img#character')
 const testChar = document.querySelector('img#test-sprite')
+const battleButtons = document.querySelector('div#battle-buttons')
 let position = { x: 1, y: 1 }
-let wallArray = { x: [10, 1, 2, 3, 4, 6, 7, 8, 10, 6, 7, 8, 10, 2, 3, 4, 6, 8, 10, 2, 3, 8, 10, 2, 3, 5, 6, 8, 10, 2, 6, 6, 8, 9, 1, 2, 3, 5, 6, 7, 8, 9], 
-    y: [1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9] }
+let wallArray = {
+    x: [10, 1, 2, 3, 4, 6, 7, 8, 10, 6, 7, 8, 10, 2, 3, 4, 6, 8, 10, 2, 3, 8, 10, 2, 3, 5, 6, 8, 10, 2, 6, 6, 8, 9, 1, 2, 3, 5, 6, 7, 8, 9],
+    y: [1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9]
+}
 
 const logBox = document.querySelector('div#log')
 const battleCommand = document.querySelector('div#battle-command')
@@ -18,17 +21,43 @@ function createWalls() {
 
         const wallDiv = document.querySelector(`div#gi${wallArray.x[i]}-${wallArray.y[i]}`)
         wallDiv.classList.add("wall")
-        console.log(wallDiv)
     }
 }
 
-document.addEventListener('click', () => {startBattle()})
+document.addEventListener('click', () => { startBattle() })
 // startBattle should take 2 args. Player and Enemy
 function startBattle(id) {
     mapContainer.style.display == "grid" ? mapContainer.style.display = "none" : mapContainer.style.display = "grid"
     battleContainer.style.display == "none" ? battleContainer.style.display = "block" : battleContainer.style.display = "none"
     battleCommand.style.display == "none" ? battleCommand.style.display = "block" : battleCommand.style.display = "none"
 
+}
+
+battleButtons.addEventListener('click', e => {
+    const enemyId = testChar.closest('div').dataset.id
+    switch (e.target.id) {
+        case "attack":
+            battleAttack(enemyId)
+            break;
+        case "special":
+            battleSpecial(enemyId)
+            break;
+        case "battle-item":
+            showBattleItems()
+            break;
+        case "run":
+            battleRun()
+            break;
+    }
+})
+
+function battleAttack(id) {
+    const attackMultiplier = ((PLAYER.level - 1) * .25) + 1
+    fetch(`http://localhost:3000/enemies/${id}`)
+        .then(res => res.json)
+        .then(enemy => {
+            ///// CHANGE ENEMY HP, CHECK FOR -NUM or 0 
+        })
 }
 
 // DOM CONTENT
@@ -87,7 +116,7 @@ function checkWall(direction) {
                 return false
             }
 
-        case "down": 
+        case "down":
             let checkDown = document.querySelector(`div#gi${position.x}-${position.y + 1}`)
             if (checkDown) {
                 return checkDown.classList.contains('wall') ? false : true
@@ -99,12 +128,12 @@ function checkWall(direction) {
 
 function checkGrid() {
     const targetDiv = testChar.closest('div')
-    if (testChar.closest('div').classList.contains('treasure')) {    
-        const id = targetDiv.dataset.id      
+    if (testChar.closest('div').classList.contains('treasure')) {
+        const id = parseInt(targetDiv.dataset.id)
         pickupTreasure(id)
     }
     else if (testChar.closest('div').classList.contains('enemy')) {
-        const id = targetDiv.dataset.id
+        const id = parseInt(targetDiv.dataset.id)
         startBattle(id)
     }
 }
@@ -117,7 +146,6 @@ function moveLeft() {
     }
     checkGrid()
 }
-
 
 function moveRight() {
     if (checkWall('right')) {
@@ -149,7 +177,7 @@ function moveDown() {
 function moveCharacter(e) {
     switch (e.keyCode) {
         case 37: // Left
-        testChar.className = "facing_left"
+            testChar.className = "facing_left"
             moveLeft()
             break;
 
@@ -158,7 +186,7 @@ function moveCharacter(e) {
             break;
 
         case 39: // Right 
-        testChar.className = "facing_right"
+            testChar.className = "facing_right"
             moveRight()
             break;
 
@@ -173,39 +201,46 @@ function moveCharacter(e) {
 
 
 function spawnEnemies() {
-    enemyObj = 
-    {x: [5, 1, 3, 5, 7, 10, 9, 9],
-    y: [1, 5, 10, 4, 5, 7, 1, 5]}
+    enemyObj =
+    {
+        x: [5, 1, 3, 5, 7, 10, 9, 9],
+        y: [1, 5, 10, 4, 5, 7, 1, 5]
+    }
     for (let i = 0; i < enemyObj.x.length; i++) {
         const spawnDiv = document.querySelector(`div#gi${enemyObj.x[i]}-${enemyObj.y[i]}`)
         spawnDiv.classList.add('enemy')
+        spawnDiv.dataset.id = `${i + 1}`
     }
 }
 
 function spawnTreasures() {
-    itemObj = 
-    {x: [1, 7, 10, 9],
-    y: [10, 4, 8, 3]}
+    itemObj =
+    {
+        x: [1, 7, 10, 9],
+        y: [10, 4, 8, 3]
+    }
     for (let i = 0; i < itemObj.x.length; i++) {
         const spawnDiv = document.querySelector(`div#gi${itemObj.x[i]}-${itemObj.y[i]}`)
         spawnDiv.classList.add('treasure')
+        spawnDiv.dataset.id = `${i + 1}`
     }
 }
 
 function pickupTreasure(id) {
-        fetch('http://localhost:3000/possessions/', {
+    fetch('http://localhost:3000/possessions/', {
         method: 'POST',
-        headers: {'Content-Type': "application/json"},
-        body: JSON.stringify({player_id: 1, item_id: id})
-        })
-        .then (res => res.json())
+        headers: { 'Content-Type': "application/json" },
+        body: JSON.stringify({ player_id: 1, item_id: id })
+    })
+        .then(res => res.json())
         .then(possession => {
             console.log(possession)
             const targetDiv = testChar.closest('div')
             targetDiv.classList.remove('treasure')
+            console.log(targetDiv)
+            logBox.textContent = `You got a ${possession.item.name}! ${possession.item.description}`
         })
 
-        logBox.textContent = "Pick up item.quantity item.name"
 
 }
 
